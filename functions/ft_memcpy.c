bbc6c3251d7787224cf5a1527157a6068a0173ea
Paixex <puto.gil.18@gmail.com>
2022-10-26 22:43:35 +0100

    questions for tomorrow

--- functions/ft_memcpy.c
+++ functions/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: digil-pa <digil-pa@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 18:00:18 by digil-pa          #+#    #+#             */
-/*   Updated: 2022/10/26 18:30:13 by digil-pa         ###   ########.fr       */
+/*   Updated: 2022/10/26 22:42:14 by digil-pa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,12 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	unsigned char	*s;
 	size_t			k;
 
+	if (!dst && !src)//
+		return (NULL);//
 	d = dest;
 	s = src;
 	k = 0;
-	while (d[k] != '\0' && k < n)
+	while (d[k] != '\0' && k < n) //tirar? "d[k] != '\0'"??
 	{
 		d[k] = s[k];
 		k++;
--- functions/ft_memmove.c
+++ functions/ft_memmove.c
@@ -6,8 +6,27 @@
 /*   By: digil-pa <digil-pa@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 18:37:34 by digil-pa          #+#    #+#             */
-/*   Updated: 2022/10/26 18:37:48 by digil-pa         ###   ########.fr       */
+/*   Updated: 2022/10/26 22:40:18 by digil-pa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <string.h>
+#include <stdlib.h>
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	unsigned char	*d;
+	unsigned char	*s;
+	size_t			k;
+
+	d = dest;
+	s = src;
+	k = 0;
+	while (d[k] != '\0' && k < n || d[k] == s[k]) //tirar? "d[k] != '\0'"??
+	{
+		d[k] = s[k];
+		k++;
+	}
+	return (d);
+}
