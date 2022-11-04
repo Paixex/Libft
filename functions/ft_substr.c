4cbfab494293290b461bc451698b983d2d35ccf5
Paixex <puto.gil.18@gmail.com>
2022-11-04 08:44:52 +0000

    substr done

--- functions/ft_substr.c
+++ functions/ft_substr.c
@@ -6,18 +6,29 @@
 /*   By: digil-pa <digil-pa@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/03 11:21:05 by digil-pa          #+#    #+#             */
-/*   Updated: 2022/11/03 12:29:07 by digil-pa         ###   ########.fr       */
+/*   Updated: 2022/11/04 08:44:39 by digil-pa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*#include "libft.h"
+#include "libft.h"
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char	*str;
+	size_t	k;
+	size_t	j;
+	char	*substr;
 
-	start = 0;
-	
-	if (start > 0)
-		return (NULL);	
-}*/
+	if (!s)
+		return (0);
+	k = 0;
+	j = start;
+	if (len > ft_strlen(s))
+		len = ft_strlen(s) + 1;
+	substr = (char *)malloc(len + 1);
+	if (!substr)
+		return (0);
+	while (j < ft_strlen(s) && k < len)
+		substr[k++] = s[j++];
+	substr[k] = '\0';
+	return (substr);
+}
