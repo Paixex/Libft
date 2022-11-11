fcd73e1721103f4ba2927249f8b29cc68b593456
Paixex <puto.gil.18@gmail.com>
2022-11-11 11:42:10 +0000

    .

--- functions/ft_itoa.c
+++ functions/ft_itoa.c
@@ -6,13 +6,38 @@
 /*   By: digil-pa <digil-pa@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 14:50:59 by digil-pa          #+#    #+#             */
-/*   Updated: 2022/11/10 14:52:59 by digil-pa         ###   ########.fr       */
+/*   Updated: 2022/11/10 18:57:21 by digil-pa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+int	ft_sinal(int n)
+{
+	int	sinal;
+
+	sinal = 0;
+	if (n < 0)
+		sinal = -1;
+	else
+		sinal = 1;
+	return (sinal);
+}
+
+int	ft_size_0(int n)
+{
+	int	size;
+
+	size = 0;
+	if (n > 0)
+		size = 0;
+	else
+		size = 1;
+	return (size);
+}
+
 char *ft_itoa(int n)
 {
+	size_t	size;
 	
 }
